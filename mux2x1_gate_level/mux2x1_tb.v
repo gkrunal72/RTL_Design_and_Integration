0df26a2850458793f2af9d9d1d55dd6381d7f1c7
gkrunal72 <gkrunal72@gmail.com>
2022-11-07 20:50:19 -0500

    Adding vcd, tb and .do file

--- mux2x1_gate_level/mux2x1_tb.v
+++ mux2x1_gate_level/mux2x1_tb.v
@@ -1,10 +1,11 @@
-`include "mux2x1.v"
+`include "mux2x1.v"         //Include DUT file to instantiate the modules
 
 module mux2x1_tb;
 
-reg i0_tb, i1_tb, sel_tb;
-wire y_tb;
+reg i0_tb, i1_tb, sel_tb;   //Inputs to the DUT (Mux2x1.v)
+wire y_tb;                  //Output of DUT
 
+//Instantiation of module - Connection by name
 mux2x1 m1 ( .i0(i0_tb), 
             .i1(i1_tb), 
             .sel(sel_tb), 
@@ -12,28 +13,26 @@ mux2x1 m1 ( .i0(i0_tb),
 
 initial begin
 
-//	$dumpfile("dump.vcd");
-//  $dumpvars(0,testbench);
-    
-    #1
-    i0_tb = 0;
-    i1_tb = 0;
-    sel_tb = 0;
-    
-    #1
-    i0_tb = 0;
-    i1_tb = 1;
-    sel_tb = 1;
-    
-    #1
-    i0_tb = 1;
-    i1_tb = 0;
-    sel_tb = 0;
+//Execute the instructions "n" (e.g. 10) number of times
+repeat (10) begin
 
-    #1
-    i0_tb = 1;
-    i1_tb = 1;
-    sel_tb = 1;
+    i0_tb = $random;        //$random - To assign random values
+    i1_tb = $random;        //In our case 0 or 1 as our variable are of 1 bit
+    sel_tb = $random;
+    #1;
+end    
+
+end
+
+initial begin
+    //Print the value of variables continuously whenever value of variable changes
+    $monitor("time - %0t, i0=%b, i1=%b, sel=%b, y=%b", $time, i0_tb, i1_tb, sel_tb, y_tb);
+end
+
+initial begin
+    
+    $dumpfile("mux.vcd"); //Value change dump file
+    $dumpvars(0,mux2x1_tb);
 end
 
 endmodule
--- mux2x1_gate_level/run.do
+++ mux2x1_gate_level/run.do
@@ -1,9 +1,13 @@
+#compilation
 vlog mux2x1_tb.v 
 
+#Elaboration
 vsim mux2x1_tb   
 
+#add signals to the waveform
 #add wave sim:/mux2x1_tb/m1/*
 do wave.do
 
+#simulation
 run -all
 
